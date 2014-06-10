1a9be12837161579cd9627e80b32b3cf13ca391d
CodeOmar <omar@evansbros.info>
2014-06-10 18:20:28 -0400

    64-bit targets only

--- Platforms/Windows/Damp Keg/Desktop Client/MainWindow.xaml.cs
+++ Platforms/Windows/Damp Keg/Desktop Client/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Desktop_Client
+namespace DampKeg
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
