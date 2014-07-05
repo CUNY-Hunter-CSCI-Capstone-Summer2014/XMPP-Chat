8c3bcd6127d14469ffbd63995a7a6610fbbc7fd8
Mark Dologuin <magliter@gmail.com>
2014-07-04 21:27:55 -0400

    GUI Stuff

---
+++ Core Library/Connection.cpp
@@ -0,0 +1,9 @@
+#include "Connection.h"
+#include <string>
+namespace DampKeg{
+
+
+	Connection::Connection(std::string host, std::string service){
+
+	}
+}
---
+++ DampKeg/Windows Desktop/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace DampKeg
+{
+    class Utility
+    {
+
+        public static bool valid_email_address(string incoming)
+        {
+            Regex rgx = new Regex(".+@.+");
+            return rgx.IsMatch(incoming);
+        }
+
+    }
+}
